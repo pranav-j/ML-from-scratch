348cc572142fa86127df4fe86354f3906c62b589
pranav-j <pranavj10@gmail.com>
2026-08-31 07:54:08 +0530

    Refactor nn_create function and update NeuraNetwork structure for clarity and consistency

--- Neural_Network_in_C/neural_network/nn.c
+++ Neural_Network_in_C/neural_network/nn.c
@@ -1,16 +1,27 @@
+#include "nn.h"
+#include "activation.h"
 #include "../matrix/matrix.h"
 #include "../matrix/operations.h"
-#include "activation.h"
 
-NeuralNetwork* create_nn(int num_inputs, int num_hiddens, int num_outputs, double learning_rate) {
+NeuralNetwork* nn_create(int num_inputs, int num_hiddens, int num_outputs, double learning_rate) {
     NeuralNetwork* nn = malloc(sizeof(NeuralNetwork));
+    if(!nn) return NULL;
+
     nn->num_inputs = num_inputs;
     nn->num_hiddens = num_hiddens;
     nn->num_outputs = num_outputs;
-    Matrix* hidden_layer = matrix_create(num_hiddens, num_inputs);
-    Matrix* output_layer = matrix_create(num_outputs, num_hiddens);
-    matrix_randomize(hidden_layer, num_hiddens);
-    matrix_randomize(output_layer, num_outputs);
+    nn->learning_rate = learning_rate;
+
+    nn->W1 = matrix_create(num_hiddens, num_inputs);
+    nn->b1 = matrix_create(num_hiddens, 1);
+    nn->W2 = matrix_create(num_outputs, num_hiddens);
+    nn->b2 = matrix_create(num_outputs, 1);
+
+    matrix_randomize(nn->W1, num_inputs);
+    matrix_init(nn->b1, 0.0);
+    matrix_randomize(nn->W2, num_hiddens);
+    matrix_init(nn->b2, 0.0);
+
     nn->hidden_weights = hidden_layer;
     nn->output_weights = output_layer;
     return nn;
--- Neural_Network_in_C/neural_network/nn.h
+++ Neural_Network_in_C/neural_network/nn.h
@@ -5,6 +5,10 @@ typedef struct {
     int num_hiddens;
     int num_outputs;
     double learning_rate;
-    Matrix* hidden_weights;
-    Matrix* output_weights;
+    Matrix* W1;     // (num_hiddens, num_inputs)
+    Matrix* b1;     // (num_hidden, 1)
+    Matrix* W2;     // (num_outputs, num_hiddens)
+    Matrix* b2;     // (num_outputs, 1)
 } NeuraNetwork;
+
+NeuralNetwork* nn_create(int num_inputs, int num_hiddens, int num_outputs, double learning_rate);
