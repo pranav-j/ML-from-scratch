a5cc15e08a55b29a3ae300bac5913431b9bc1e6a
pranav-j <pranavj10@gmail.com>
2026-09-03 07:56:18 +0530

    Implement nn_train_one function for training a single step in the Neural Network

--- Neural_Network_in_C/neural_network/nn.c
+++ Neural_Network_in_C/neural_network/nn.c
@@ -1,3 +1,7 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "nn.h"
 #include "activation.h"
 #include "../matrix/matrix.h"
@@ -48,4 +52,54 @@ void forward(NeuralNetwork* nn, Matrix* x, Matrix** z1, Matrix** a1, Matrix** z2
     *z2 = add(W2a1, nn->b2);
 
     *a2 = softmax(*z2);
+}
+
+double nn_train_one(NeuralNetwork* nn, Matrix* x, Matrix* y) {
+    Matrix *z1, *a1, *z2, *a2;
+
+    // FORWARD
+    forward(nn, x, &z1, &a1, &z2, &a2);
+
+    // LOSS
+    double loss = 0.0;
+    for(int i = 0; i < y->rows; i++) {
+        loss += -y->values[i][0] * log(a2->values[1][0]);
+    }
+
+
+    // BACKWARD
+    Matrix* dz2 = subtract(a2, y);
+
+    Matrix* dW2 = dot(dz2, transpose(a1));
+
+    Matrix* db2 = dz2;
+
+    Matrix* da1 = dot(transpose(nn->W2), dz2);
+    
+    Matrix* sp = apply(sigmoidPrime, a1);
+    Matrix* dz1 = hadamard(da1, sp);
+
+    Matrix* dW1 = dot(dz1, transpose(x));
+
+    Matrix* db1 = dz1;
+
+    // UPDATE
+    matrix_update(nn->W2, dW2, nn->learning_rate);
+    matrix_update(nn->b2, db2, nn->learning_rate);
+    matrix_update(nn->W1, dW1, nn->learning_rate);
+    matrix_update(nn->b1, db1, nn->learning_rate);
+
+    // FREE INTERMEDIARIES
+    matrix_free(z1); matrix_free(a1); matrix_free(z2); matrix_free(a2);
+    matrix_free(dz2);   // this also disposes of db2 (same pointer)
+    matrix_free(a1_T);
+    matrix_free(dW2);
+    matrix_free(W2_T);
+    matrix_free(da1);
+    matrix_free(sp);
+    matrix_free(dz1);   // this also disposes of db1 (same pointer)
+    matrix_free(x_T);
+    matrix_free(dW1);
+
+    return loss;
 }
--- Neural_Network_in_C/neural_network/nn.h
+++ Neural_Network_in_C/neural_network/nn.h
@@ -14,3 +14,4 @@ typedef struct {
 NeuralNetwork* nn_create(int num_inputs, int num_hiddens, int num_outputs, double learning_rate);
 void nn_free(NeuralNetwork* nn);
 void forward(NeuralNetwork* nn, Matrix* x, Matrix** z1, Matrix** a1, Matrix** z2, Matrix** a2);
+double nn_train_one(NeuralNetwork* nn, Matrix* x, Matrix* y);
